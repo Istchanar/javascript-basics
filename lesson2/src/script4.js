3e9d2f4659629702adf3352b67c72c3b5adc89fa
istchanar <istchanar@outlook.com>
2021-12-25 01:38:07 +0700

    Lesson 2 add

---
+++ lesson2/src/script4.js
@@ -0,0 +1,23 @@
+"use strict";
+
+    /* 
+    4. Реализовать основные 4 арифметические операции (+, -, /, *) в виде функций с 
+    двумя параметрами. Т.е. например, функция для сложения должна принимать два числа,
+    складывать их и возвращать результат. Обязательно использовать оператор return.
+    */
+
+function sumTwoNumbers(a, b){
+    return a + b;
+}
+
+function minusTwoNumber (a, b){
+    return a - b;
+}
+
+function divisionTwoNumber(a, b){
+    return a / b;
+}
+
+function multiplicationTwoNumbers(a, b){
+    return a * b;
+}
